ae698bce925ea47199c2175227f157da57c70813
Daphne Yang <daphne.yang@salesforce.com>
2024-08-15 09:47:57 -0400

    feat: add command to package.json + add command text to i18n.ts

---
+++ src/messages/i18n.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+export const messages = {
+    capture_startup_performance_command_text: "SFDX: Capture Startup Performance"
+};
