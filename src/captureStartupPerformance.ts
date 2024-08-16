5b3ba3fd1adc916927d8c8f6f58141287e0ca3da
Daphne Yang <daphne.yang@salesforce.com>
2024-08-16 17:22:38 -0400

    chore: pseudocode for captureStartupPerformance() method

--- src/captureStartupPerformance.ts
+++ src/captureStartupPerformance.ts
@@ -12,4 +12,16 @@ export class CaptureStartupPerformance {
 
 export const captureStartupPerformance = async (): Promise<void> => {
   console.log('Method - This is the Capture Startup Performance class!');
+  // TODO: create dummy functions for the following in the scaffolding:
+  // open Developer: Startup Performance (open via API - try "perfview.show")
+  // save startup performance file (TODO: how to do this without popup dialog?)
+  // read startup performance file using fs.readFileSync()
+  // parse startup performance file using marked
+  // save parsed results to a file
+  // send parsed results to appinsights
+
+  // TODO: also need to communicate with the customer
+    // is it possible to block the UI without also blocking what we need to do?
+    // is it possible to open a file but keep it hidden?
+    // TODO: schedule time with Meg after everything except telemetry is working and it can be demoed
 };
