52b33cef115c70125f2f7c2ad203b48ee7869d57
Daphne Yang <daphne.yang@salesforce.com>
2024-08-19 10:36:32 -0400

    feat: add helper functions for pseudocode

--- src/captureStartupPerformance.ts
+++ src/captureStartupPerformance.ts
@@ -12,16 +12,42 @@ export class CaptureStartupPerformance {
 
 export const captureStartupPerformance = async (): Promise<void> => {
   console.log('Method - This is the Capture Startup Performance class!');
-  // TODO: create dummy functions for the following in the scaffolding:
   // open Developer: Startup Performance (open via API - try "perfview.show")
+  await openStartupPerformanceFile();
   // save startup performance file (TODO: how to do this without popup dialog?)
+  const fileName = await saveCurrentFile();
   // read startup performance file using fs.readFileSync()
+  const fileContents = await readStartupPerformanceFile(fileName);
   // parse startup performance file using marked
+  const parsedResults = await parseStartupPerformanceFile(fileContents);
   // save parsed results to a file
+  await copyContentsToFile(parsedResults);
+  const parsedResultsFile = await saveCurrentFile();
   // send parsed results to appinsights
+  await sendToAppInsights(parsedResultsFile);
 
   // TODO: also need to communicate with the customer
     // is it possible to block the UI without also blocking what we need to do?
     // is it possible to open a file but keep it hidden?
     // TODO: schedule time with Meg after everything except telemetry is working and it can be demoed
 };
+
+// ------- Helper Functions -------
+
+const openStartupPerformanceFile = async(): Promise<void> => {}
+
+const saveCurrentFile = async(): Promise<string> => {
+  return 'filename of saved file';
+}
+
+const readStartupPerformanceFile = async(fileName: string): Promise<string> => {
+  return 'contents of startup performance file';
+}
+
+const parseStartupPerformanceFile = async(fileContents: string): Promise<string> => {
+  return 'parsed contents of Extension Activation Stats table';
+}
+
+const copyContentsToFile = async(contents: string): Promise<void> => {}
+
+const sendToAppInsights = async(fileName: string): Promise<void> => {}
