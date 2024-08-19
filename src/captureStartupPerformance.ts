edac345cd7f35998b948091868a26fd7619b0f53
Daphne Yang <daphne.yang@salesforce.com>
2024-08-19 17:26:59 -0400

    feat: in progress and success notifications

--- src/captureStartupPerformance.ts
+++ src/captureStartupPerformance.ts
@@ -4,6 +4,10 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+import * as vscode from 'vscode';
+import { delay } from 'lodash';
+
 export class CaptureStartupPerformance {
   constructor() {
     console.log('Constructor - This is the Capture Startup Performance class!');
@@ -12,19 +16,34 @@ export class CaptureStartupPerformance {
 
 export const captureStartupPerformance = async (): Promise<void> => {
   console.log('Method - This is the Capture Startup Performance class!');
-  // open Developer: Startup Performance (open via API - try "perfview.show")
-  await openStartupPerformanceFile();
-  // save startup performance file (TODO: how to do this without popup dialog?)
-  const fileName = await saveCurrentFile();
-  // read startup performance file using fs.readFileSync()
-  const fileContents = await readStartupPerformanceFile(fileName);
-  // parse startup performance file using marked
-  const parsedResults = await parseStartupPerformanceFile(fileContents);
-  // save parsed results to a file
-  await copyContentsToFile(parsedResults);
-  const parsedResultsFile = await saveCurrentFile();
-  // send parsed results to appinsights
-  await sendToAppInsights(parsedResultsFile);
+
+  // Show running notification to the user
+  await vscode.window.withProgress({
+    location: vscode.ProgressLocation.Notification,
+    title: 'SFDX: Capture Startup Performance',
+    cancellable: false
+    }, async (progress) => {
+    progress.report({ message: 'Running...' });
+
+    // open Developer: Startup Performance (open via API - try "perfview.show")
+    await openStartupPerformanceFile();
+    // save startup performance file (TODO: how to do this without popup dialog?)
+    const fileName = await saveCurrentFile();
+    // read startup performance file using fs.readFileSync()
+    const fileContents = await readStartupPerformanceFile(fileName);
+    // parse startup performance file using marked
+    const parsedResults = await parseStartupPerformanceFile(fileContents);
+    // save parsed results to a file
+    await copyContentsToFile(parsedResults);
+    const parsedResultsFile = await saveCurrentFile();
+    // send parsed results to appinsights
+    await sendToAppInsights(parsedResultsFile);
+
+    await sleep(5000);
+  });
+
+  // Show success notification to the user
+  vscode.window.showInformationMessage('SFDX: Capture Startup Performance command completed successfully.');
 
   // TODO: also need to communicate with the customer
     // is it possible to block the UI without also blocking what we need to do?
@@ -51,3 +70,8 @@ const parseStartupPerformanceFile = async(fileContents: string): Promise<string>
 const copyContentsToFile = async(contents: string): Promise<void> => {}
 
 const sendToAppInsights = async(fileName: string): Promise<void> => {}
+
+const sleep = (ms: number): Promise<void> => {
+  return new Promise(resolve => delay(resolve, ms));
+};
+
